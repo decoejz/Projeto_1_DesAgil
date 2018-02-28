31c8b83a2894ed24faaa60bdb1b950971f0c0e3d
decoejz <decoejz@gmail.com>
2018-02-28 20:49:16 -0300

    add board e jogadores

---
+++ AndreEjzenmesser/src/main/java/br/pro/hashi/ensino/desagil/tequilada/CpuPlayer.java
@@ -0,0 +1,6 @@
+package br.pro.hashi.ensino.desagil.tequilada;
+
+public class CpuPlayer {
+	int x;
+	int y;
+}
---
+++ AndreEjzenmesser/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Engine.java
@@ -0,0 +1,7 @@
+package br.pro.hashi.ensino.desagil.tequilada;
+
+public class Engine {
+	Board board = new Board();
+	CpuPlayer cpuPlayer = new CpuPlayer();
+	HumanPlayer humanPlayer = new HumanPlayer();
+}
